5c4f782992b1c99ee225cc54ae7fb8dc1243ccc8
cjw9770@gmail.com <cjw9770@gmail.com>
2026-05-15 17:55:01 +0000

    feat(preprocessing): add scene detection

--- src/v2a_inspect/preprocessing/__init__.py
+++ src/v2a_inspect/preprocessing/__init__.py
@@ -1,6 +1,8 @@
+from .scenes import detect_initial_scenes
 from .video import normalize_video, prepare_video
 
 __all__ = [
+    "detect_initial_scenes",
     "normalize_video",
     "prepare_video",
 ]
