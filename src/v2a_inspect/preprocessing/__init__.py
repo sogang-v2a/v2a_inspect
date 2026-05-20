50ae0bc9ef51e0a604f0d251b007df040a0fc170
cjw9770@gmail.com <cjw9770@gmail.com>
2026-05-20 04:34:00 +0000

    feat(preprocessing): add pipeline wrapper

--- src/v2a_inspect/preprocessing/__init__.py
+++ src/v2a_inspect/preprocessing/__init__.py
@@ -3,6 +3,7 @@ from .keyframes import (
     extract_keyframes_for_initial_scenes,
     select_initial_scene_keyframe_indexes,
 )
+from .pipeline import preprocess_video
 from .scenes import detect_initial_scenes
 from .video import normalize_video, prepare_video
 
@@ -12,5 +13,6 @@ __all__ = [
     "extract_keyframes_for_initial_scenes",
     "normalize_video",
     "prepare_video",
+    "preprocess_video",
     "select_initial_scene_keyframe_indexes",
 ]
